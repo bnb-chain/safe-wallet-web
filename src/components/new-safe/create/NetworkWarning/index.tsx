bc4c61fdaab95b11c65ec71b3948338531e57759
devinxl <devin@nodereal.io>
2025-08-22 10:55:45 +0800

    feat: direct opBNB users to app.safe.global in NetworkWarning

--- src/components/new-safe/create/NetworkWarning/index.tsx
+++ src/components/new-safe/create/NetworkWarning/index.tsx
@@ -2,6 +2,7 @@ import { Alert, AlertTitle, Box } from '@mui/material'
 import { useCurrentChain } from '@/hooks/useChains'
 import ChainSwitcher from '@/components/common/ChainSwitcher'
 import { useIsUnsupportedChain } from '@/hooks/useIsUnsupportedChain'
+import ExternalLink from '@/components/common/ExternalLink'
 
 const NetworkWarning = () => {
   const chain = useCurrentChain()
@@ -9,10 +10,19 @@ const NetworkWarning = () => {
 
   if (!chain) return null
 
+  // Check if current chain is opBNB (chain ID 204)
+  const isOpBNB = Number(chain.chainId) === 204
+
   const message = isUnsupportedChain ? (
     <>
-      <strong>Unfortunately, creating a Safe Account on {chain.chainName} is disabled. </strong> Please switch to a
-      supported network.
+      <strong>Unfortunately, creating a Safe Account on {chain.chainName} is disabled. </strong>
+      {isOpBNB ? (
+        <>
+          Please use <ExternalLink href="https://app.safe.global/">app.safe.global</ExternalLink>.
+        </>
+      ) : (
+        'Please switch to a supported network.'
+      )}
     </>
   ) : (
     `You are trying to create a Safe Account on ${chain.chainName}. Make sure that your wallet is set to the same
